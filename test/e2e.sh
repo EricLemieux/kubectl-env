b70d193fb4ab1f4c676a35aa3f2fa81a0085f1bd
Eric Lemieux <eric@lemieuxdev.com>
2020-04-17 22:13:53 -0400

    Adding container selector

--- test/e2e.sh
+++ test/e2e.sh
@@ -9,6 +9,7 @@ assert_contains() {
   else
     echo "✔ - ${1}"
   fi
+  echo "------------------------------"
 }
 
 setup() {
@@ -25,11 +26,19 @@ main() {
   setup
 
   pod=$(kubectl get pod -l app=example-deployment -o name)
-  ns_pod=$(kubectl get pod -l app=example-deployment -o name -n my-namespace)
 
   assert_contains "Contains an error message when missing a pod" "$(./kubectl-env 2>&1)" "ERROR: Pod required"
   assert_contains "Finds env values from pod" "$(./kubectl-env "${pod}")" "ABC=abc"
+
+  # Namspace selector
+  ns_pod=$(kubectl get pod -l app=example-deployment -o name -n my-namespace)
   assert_contains "Find env values in secondary namespace" "$(./kubectl-env "${ns_pod}" -n "${secondary_namespace}")" "ABC=abc"
+
+  # Container selector
+  container_pod=$(kubectl get pod -l app=example-multi-container -o name)
+  assert_contains "Finds values in the primary container with no extra specificity" "$(./kubectl-env "${container_pod}")" "ABC=abc"
+  assert_contains "Finds values in the primary container with container selector" "$(./kubectl-env "${container_pod}" -c "primary-container")" "ABC=abc"
+  assert_contains "Finds values in the secondary container with container selector" "$(./kubectl-env "${container_pod}" -c "secondary-container")" "ABC=aaa"
 }
 
 main "${@}"
